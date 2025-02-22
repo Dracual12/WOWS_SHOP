2e2870c9c77422ca681bee54d5491f5b0b5092fe
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-02-22 03:48:24 +0300

    5

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "7574071837:AAFE0A2rW27YmxOi40AG68577fK3zluinu4"
 ADMIN_ID = 1456241115
-WEB_APP_URL = "https://80b8-2001-ac8-31-330-9450-c80d-db2b-9.ngrok-free.app"
+WEB_APP_URL = "www.armada-wows-shop.ru"
