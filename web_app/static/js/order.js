c3c536302e42511244c6e12963d5ec766a6e7598
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-01 02:53:33 +0300

    c c

--- web_app/static/js/order.js
+++ web_app/static/js/order.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     checkoutButton.addEventListener("click", async () => {
      if (window.Telegram && window.Telegram.WebApp) {
         console.log("Telegram WebApp script is loaded!");
-        alert(window.Telegram.WebApp.initDataUnsafe.user.id);
+        alert(window.Telegram.WebApp.initDataUnsafe.user.id);}
         // Выводим Telegram ID в консоль сервера, отправив его через fetch
         // Закрываем окно корзины
         const cartDropdown = document.querySelector('.cart-dropdown');
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Показываем всплывающее окно оформления заказа (первый шаг)
         showOrderPopup();
-    };
+    });
 
     function showOrderPopup() {
         const popup = document.createElement("div");
