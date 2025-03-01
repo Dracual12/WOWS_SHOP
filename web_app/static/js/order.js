643e910524927f266b4656ac290d05d48a77389e
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-01 03:03:04 +0300

    fvf

--- web_app/static/js/order.js
+++ web_app/static/js/order.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     checkoutButton.addEventListener("click", async () => {
     if (window.Telegram && window.Telegram.WebApp) {
         console.log("Telegram WebApp script is loaded!");
-        (window.Telegram.WebApp.initDataUnsafe.user.id);}
+        alert(window.Telegram.WebApp.initDataUnsafe.user.id);}
         // Выводим Telegram ID в консоль сервера, отправив его через fetch
 
 
