1aae3ee05068579b54ed92aa888c6a6276d30d9f
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-01 18:59:35 +0300

    km

--- web_app/static/js/order.js
+++ web_app/static/js/order.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ telegram_id: userId })
-        }
+        })
         });
     }
 });
