af785a85add22963ce82bac6c8c1c1d31e01b72a
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-01 03:26:27 +0300

    c cdc

--- web_app/app.py
+++ web_app/app.py
@@ -27,12 +27,11 @@ def index():
 
 @app.route("/save-tg-id", methods=["POST"])
 def save_tg_id():
-    global tg_id
     data = request.get_json()  # Получаем данные из запроса
     tg_id = data.get("tg_id")  # Извлекаем Telegram ID
     print(tg_id)
     # Возвращаем ответ клиенту
-    return tg_id
+    return jsonify({"message": f"Telegram ID {tg_id} сохранён"})
 
 
 
