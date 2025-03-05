2571972b073b4be568a147db47ad0bf73df6a34e
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-05 17:07:17 +0300

    мама

--- bot/main.py
+++ bot/main.py
@@ -83,7 +83,7 @@ async def get_link(user):
     k  = response.json()
     print(k)
     a = k["formUrl"]
-    message_obj = await botik.send_message(user, text=f"Нажимая <b>Оплатить</b> Вы принимаете пользовательское соглашение", reply_markup=pay(a))
+    message_obj = await botik.send_message(user, text=f"Нажимая «Оплатить» Вы принимаете пользовательское соглашение", reply_markup=pay(a))
     conn = get_db_connection()
     order_message_id = conn.execute('UPDATE users SET message_id = ? WHERE telegram_id = ?', (message_obj.message_id, user))
     conn.close()
