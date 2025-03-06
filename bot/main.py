4f0a0c4141c6eac86d85277a9ba64804897ed6ba
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-06 09:40:13 +0300

    jn

--- bot/main.py
+++ bot/main.py
@@ -80,8 +80,9 @@ async def get_link(user):
     conn.close()
     url = f"https://payment.alfabank.ru/payment/rest/register.do?token=oj5skop8tcf9a8mmoh9ssb31ei&orderNumber={order_id}&amount={cart}&returnUrl=https://t.me/armada_gold_bot"
     response = requests.get(url)
+
     k  = response.json()
-    print(k)
+    print(order_id)
     if 'formUrl' in k:
         a = k['formUrl']
         message_obj = await botik.send_message(user,
