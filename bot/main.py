8c668fb8645564692f7287d14e12d5bef44d38a7
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-08 07:37:17 +0300

    cdcd

--- bot/main.py
+++ bot/main.py
@@ -78,7 +78,7 @@ async def get_link(user):
     last_cart = dict(last_cart)
     cart = int((last_cart['cart'].split('Итого:')[1]).split()[0])
     conn.close()
-    url = f"https://payment.alfabank.ru/payment/rest/register.do?token=oj5skop8tcf9a8mmoh9ssb31ei&orderNumber={order_id}&amount={cart}&returnUrl=https://t.me/armada_gold_bot"
+    url = f"https://payment.alfabank.ru/payment/rest/register.do?token=oj5skop8tcf9a8mmoh9ssb31ei&orderNumber={order_id}&amount={cart*100}&returnUrl=https://t.me/armada_gold_bot"
     response = requests.get(url)
 
     k  = response.json()
