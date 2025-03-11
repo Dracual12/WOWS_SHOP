81b676243f7ddd499bf1b360db47d76bfb4d66da
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-11 18:43:22 +0300

    rfr

--- start.py
+++ start.py
@@ -6,15 +6,18 @@ asyncio.set_event_loop(loop)
 
 # Функция для получения общего цикла
 def get_shared_loop():
+    global loop
     print("Shared loop is running:", loop.is_running())
     return loop
 
 # Функция для запуска цикла
 def run_loop():
+    global loop
     print("Starting shared loop...")
     loop.run_forever()
 
 # Функция для остановки цикла
 def stop_loop():
+    global loop
     print("Stopping shared loop...")
     loop.stop()
