2d52450ccca2f58b842c21762902a7b7d9fca8be
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-11 19:06:59 +0300

    v v

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 import asyncio
 import threading
 
+print("Initializing shared loop...")
+
 # Создаем общий цикл событий
 loop = asyncio.new_event_loop()
 
 # Функция для запуска цикла событий в отдельном потоке
 def start_loop():
+    print("Starting event loop in a separate thread...")
     asyncio.set_event_loop(loop)
     loop.run_forever()
 
@@ -14,4 +17,5 @@ threading.Thread(target=start_loop, daemon=True).start()
 
 # Функция для получения общего цикла
 def get_shared_loop():
+    print("Getting shared loop...")
     return loop
