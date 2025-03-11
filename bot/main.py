9696144ebabe94955ea0ace0c0fe6d01dbd456f7
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-11 11:47:17 +0300

    kjn

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,9 @@
+bot_loop = None
 import json
 import sys
 import os
 import time
 
-bot_loop = None
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
     sys.path.append(project_root)
