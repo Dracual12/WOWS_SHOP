bbb600159fe00cb60809577617958e522b92b116
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-03-27 06:28:57 +0300

    test2

--- web_app/config.py
+++ web_app/config.py
@@ -1,18 +1,26 @@
 import os
 
-# Пути
-BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-DB_PATH = os.path.join(BASE_DIR, 'bot', 'wows_db.db')
-
-# Конфигурация Flask
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
-DEBUG = os.environ.get('FLASK_DEBUG', 'True').lower() == 'true'
-
-# Конфигурация Telegram
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-CHAT_ID = os.environ.get('CHAT_ID')
-
-# Конфигурация базы данных
-DATABASE = {
-    'path': DB_PATH
-} 
+class Config:
+    # Пути
+    BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    DB_PATH = os.path.join(BASE_DIR, 'bot', 'wows_db.db')
+    
+    # Конфигурация Flask
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
+    DEBUG = os.environ.get('FLASK_DEBUG', 'True').lower() == 'true'
+    
+    # Конфигурация базы данных
+    DATABASE = {
+        'path': DB_PATH
+    }
+    
+    # Конфигурация Telegram
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    CHAT_ID = os.environ.get('CHAT_ID')
+    
+    # Конфигурация Альфа-банка
+    ALFABANK_TOKEN = os.environ.get('ALFABANK_TOKEN')
+    
+    # Настройки для загрузки файлов
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'bot', 'assets')
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max-limit 
