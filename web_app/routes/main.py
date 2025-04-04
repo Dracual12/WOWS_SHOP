442e9e170865355553e1664e2470557e412d3df5
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-04-04 07:56:19 +0300

    Update main.py

--- web_app/routes/main.py
+++ web_app/routes/main.py
@@ -5,7 +5,7 @@ from ..utils.telegram import send_telegram
 from ..config import Config
 bp = Blueprint('main', __name__)
 from bot.db import get_db_connection
-
+import requests
 
 def pay(link):
     keyboard = {
